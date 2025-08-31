a6f951745025cd406fcbef86ab451ecd213fabe5
hagoel <danilo.guk1@gmail.com>
2025-08-31 23:29:53 +0300

    feat: update default font and size in settings

--- src/services/SettingsManager.h
+++ src/services/SettingsManager.h
@@ -9,8 +9,8 @@ struct AppSettings {
     std::filesystem::path gameDataPath = executablePath / "game_data";
     std::filesystem::path defaultProjectPath = executablePath / "projects";
     std::string themeName = "Dark"; //todo
-    std::string fontName = "NotoSans-Regular.ttf"; // must match ImFont debug name
-    float fontSize = 20;
+    std::string fontName = "DroidSans.ttf"; // must match ImFont debug name
+    float fontSize = 18;
     bool autoSaveEnabled = true; // todo
     int autoSaveIntervalMinutes = 5; //todo
     int maxUndoHistory = 100;
